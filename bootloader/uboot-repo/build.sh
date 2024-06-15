2c56a9e5a8920aedad4ee8129925b4a39a452ba2
alex <344039851@qq.com>
2024-06-15 23:30:04 +0800

    uboot modify

--- bootloader/uboot-repo/build.sh
+++ bootloader/uboot-repo/build.sh
@@ -3,7 +3,7 @@ project_path=x2_m401a
 
 ./mk $uboot_name --systemroot;
 
-cp build/u-boot.bin ../../device/bananapi/$project_path/bootloader.img;
-cp build/u-boot.bin.usb.bl2 ../../device/bananapi/$project_path/upgrade/u-boot.bin.usb.bl2;
-cp build/u-boot.bin.usb.tpl ../../device/bananapi/$project_path/upgrade/u-boot.bin.usb.tpl;
-cp build/u-boot.bin.sd.bin ../../device/bananapi/$project_path/upgrade/u-boot.bin.sd.bin;
+#cp build/u-boot.bin ../../device/bananapi/$project_path/bootloader.img;
+#cp build/u-boot.bin.usb.bl2 ../../device/bananapi/$project_path/upgrade/u-boot.bin.usb.bl2;
+#cp build/u-boot.bin.usb.tpl ../../device/bananapi/$project_path/upgrade/u-boot.bin.usb.tpl;
+#cp build/u-boot.bin.sd.bin ../../device/bananapi/$project_path/upgrade/u-boot.bin.sd.bin;
