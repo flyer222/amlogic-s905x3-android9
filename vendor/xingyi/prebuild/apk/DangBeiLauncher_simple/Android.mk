6a87b8a1f6e3b84c0a1ee3874a7444b3a5dc44ca
alex <344039851@qq.com>
2024-06-19 21:39:26 +0800

    add danbei launcher simple version

---
+++ vendor/xingyi/prebuild/apk/DangBeiLauncher_simple/Android.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2012 Broadcom Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+LOCAL_MODULE := DangBeiLauncher_simple
+
+LOCAL_SRC_FILES := dbzm_v4.1.7_revision_AC.apk
+
+LOCAL_MODULE_TARGET_ARCH := arm
+LOCAL_PREBUILT_JNI_LIBS := \
+	lib/armeabi-v7a/libboost_multidex.so
+
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2
+
+include $(BUILD_PREBUILT)
+
