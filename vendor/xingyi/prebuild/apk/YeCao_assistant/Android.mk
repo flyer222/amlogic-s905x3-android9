80e3b24d58daa264d46b1a4f6e470bd5bf86325c
alex <344039851@qq.com>
2024-06-27 17:53:05 +0800

    add yecao apk support

---
+++ vendor/xingyi/prebuild/apk/YeCao_assistant/Android.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2012 Broadcom Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+LOCAL_MODULE := yecao-assi
+
+LOCAL_SRC_FILES := yecao-assistant.apk
+
+LOCAL_MODULE_TARGET_ARCH := arm
+LOCAL_PREBUILT_JNI_LIBS := \
+			lib/armeabi-v7a/libboost_multidex.so \
+			lib/armeabi-v7a/libepic.so \
+			lib/armeabi-v7a/libframesequence.so \
+			lib/armeabi-v7a/libgiflib.so \
+			lib/armeabi-v7a/liblamemp3.so \
+			lib/armeabi-v7a/librsjni_androidx.so \
+			lib/armeabi-v7a/librsjni.so \
+			lib/armeabi-v7a/libRSSupport.so \
+			lib/armeabi-v7a/libva++.so
+
+include $(BUILD_PREBUILT)
