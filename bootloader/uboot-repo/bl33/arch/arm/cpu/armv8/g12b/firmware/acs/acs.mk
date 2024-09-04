a46e16dadaa2dd0f9df2666a924ebeeb65d0f623
alex <344039851@qq.com>
2024-09-04 20:27:39 +0800

    split common and uboot  repository

--- bootloader/uboot-repo/bl33/arch/arm/cpu/armv8/g12b/firmware/acs/acs.mk
+++
@@ -1,9 +0,0 @@
-SOURCES		+=	acs_entry.S
-
-ifdef CONFIG_MDUMP_COMPRESS
-SOURCES 	+=	ramdump.c
-endif
-
-SOURCES		+=	acs.c
-
-LINKERFILE_T		:=	acs.ld.S
--- common/arch/arm/mach-meson/Makefile.boot
+++
@@ -1,3 +0,0 @@
-   zreladdr-y    += 0x00108000
-params_phys-y    := 0x00100100
-initrd_phys-y    := 0x00a00000
